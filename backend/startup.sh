90a1e7450c40cf3b7e8193bd7318e58e449f84f7
kacijcox <145575906+kacijcox@users.noreply.github.com>
2025-01-03 17:49:32 +0000

    startup

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,5 +1,6 @@
 # backend/startup.sh
 #!/bin/bash
 cd /home/site/wwwroot
-gunicorn --bind=0.0.0.0 --timeout 600 src.api.routes:app &
+gunicorn --bind=0.0.0.0:8000 "src.api.routes:app" &
 python monitor.py
+
