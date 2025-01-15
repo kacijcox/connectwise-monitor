69a6384721f953fc2f09114fefc5699e8ce63ded
kacijcox <145575906+kacijcox@users.noreply.github.com>
2025-01-15 03:04:29 +0000

    frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://connectwise-monitor-ategh4fmh7cmfsdu.centralus-01.azurewebsites.net/',  
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+  base: '/',
 })
